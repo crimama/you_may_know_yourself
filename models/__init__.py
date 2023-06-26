16d09ba74732987df1705f734ae561a0d3cb8939
crimama2 <crimama2@gmail.com>
2023-06-26 02:53:32 +0000

    [feat] add BeYourOwnTeacher model

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
-from .model import * 
+from .model import BaseModel 
+from .be_your_own_teacher import BeYourOwnTeacher
 
-def load_model(model_name,pretrained,num_class):
-    model = BaseModel(
-        model_name = model_name,
-        num_class  = num_class,
-        pretrained = pretrained
-    )
+def load_model(model_name, base_model, pretrained, num_class):
+    model = __import__('models').__dict__[model_name](  model_name = base_model,
+                                                                pretrained = pretrained,
+                                                                num_class  = num_class)
     return model 
